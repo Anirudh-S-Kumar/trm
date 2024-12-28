6c807ed9ec78e156325cb0c0f861b5c115ab4485
Anirudh S. Kumar <anirudh.skumar.03@gmail.com>
2024-12-29 00:44:17 +0530

    added undo, and integration with list and also formatted code

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,31 @@
 mod trm;
 
-use trm::*;
-use std::path::PathBuf;
 use clap::Parser;
-
-
+use std::path::PathBuf;
+use trm::*;
 
 fn main() {
     let args = Args::parse();
 
     let mut files: Vec<PathBuf> = vec![];
-    
-    if args.debug{
+
+    if args.debug {
         println!("Number of args received: {}", args.files.len());
     }
 
-    for file in &args.files{
+    for file in &args.files {
         files.push(PathBuf::from(file));
     }
 
-    
-
-    match setup_logging(){
-        Ok(_) => {},
+    match setup_logging() {
+        Ok(_) => {}
         Err(e) => {
             eprintln!("Could not setup logging: {}", e);
             std::process::exit(1);
         }
     }
 
-
-    let dir_path = match setup_directory(&args){
+    let dir_path = match setup_directory(&args) {
         Ok(path) => path,
         Err(e) => {
             eprintln!("Could not create directory: {}", e);
@@ -38,15 +33,18 @@ fn main() {
         }
     };
 
-    if args.verbose{
-        display_files(&files, false);
-    }
-
-    if args.list{
-        list_delete_files(&args, &dir_path, &mut files);
-    } else{
+    if args.list && args.undo {
+        let deleted_files = list_delete_files(&args, &dir_path, &mut files, true).unwrap();
+        let mut flattened_files: Vec<PathBuf> = deleted_files.into_iter().flatten().collect();
+        recover_files(&args, &dir_path, &mut flattened_files, true);
+    } else if args.list {
+        if let Err(e) = list_delete_files(&args, &dir_path, &mut files, false) {
+            eprintln!("Error listing or deleting files: {}", e);
+            std::process::exit(1);
+        }
+    } else if args.undo {
+        recover_files(&args, &dir_path, &mut files, false);
+    } else {
         move_files(&args, &dir_path, &files);
     }
-
-
 }
--- src/trm.rs
+++ src/trm.rs
@@ -1,20 +1,21 @@
-use std::fs;
-use std::{io::{self, Error}, path::PathBuf};
 use chrono::{DateTime, Local};
 use clap::Parser;
 use lscolors::LsColors;
+use std::fs;
+use std::{
+    io::{self, Error},
+    path::PathBuf,
+};
 use term_grid::{Grid, GridOptions};
 
 pub static DEFAULT_DIR: &str = "/var/tmp/trm_files";
-    pub static LOG_DIR: &str = "/var/log/trm";
-
+pub static LOG_DIR: &str = "/var/log/trm";
 
 #[derive(Parser, Debug, Default)]
 #[command(version, about = "trm - Temporary rm, a utility to reversibly remove your files", long_about=None)]
 #[command(arg_required_else_help(true))]
-pub struct Args{
+pub struct Args {
     /// Files to delete    
-    // #[arg(required_unless_present = "undo", required_unless_present="history", num_args = 0..)]
     #[arg(required_unless_present="list", num_args = 0..)]
     pub files: Vec<String>,
 
@@ -26,21 +27,26 @@ pub struct Args{
     #[arg(long)]
     pub debug: bool,
 
-    /// Display all files trashed under current directory
+    // Recover files from the trash
+    #[arg(short, long)]
+    pub undo: bool,
+
+    /// Display all files trashed under given directories.
+    /// Takes current directory as default if no other directory given
     #[arg(short, long)]
     pub list: bool,
 
     /// Directory where to move
     #[arg(short, long, default_value_t = DEFAULT_DIR.to_string())]
-    pub dir: String
+    pub dir: String,
 }
 
-struct _FileInfo{
+struct _FileInfo {
     /// The original path from where the path was copied
     path: String,
 
     /// The datetime when it was moved
-    moved_time: DateTime<Local>
+    moved_time: DateTime<Local>,
 }
 
 macro_rules! get_file_name {
@@ -49,43 +55,46 @@ macro_rules! get_file_name {
     };
 }
 
-pub fn display_files(files: &Vec<PathBuf>, only_filename: bool){
+pub fn display_files(files: &Vec<PathBuf>, only_filename: bool) {
     let lscolors = LsColors::from_env().unwrap_or_default();
     let stdout_width = terminal_size::terminal_size_of(io::stdout())
-                                                            .map(|(w, _h)| w.0 as _)
-                                                            .unwrap_or(80);
+        .map(|(w, _h)| w.0 as _)
+        .unwrap_or(80);
 
-    let file_names : Vec<String> = files.iter().map(|file|{
-        if let Some(style) = lscolors.style_for_path(&file){
-            let crossterm_style = style.to_crossterm_style();
-            if only_filename{
-                return crossterm_style.apply(get_file_name!(file)).to_string();
+    let file_names: Vec<String> = files
+        .iter()
+        .map(|file| {
+            if let Some(style) = lscolors.style_for_path(&file) {
+                let crossterm_style = style.to_crossterm_style();
+                if only_filename {
+                    return crossterm_style.apply(get_file_name!(file)).to_string();
+                }
+                crossterm_style
+                    .apply(file.display().to_string())
+                    .to_string()
+            } else {
+                if only_filename {
+                    return get_file_name!(file);
+                }
+                file.display().to_string()
             }
-            crossterm_style.apply(file.display().to_string()).to_string()
-        } else{
-            if only_filename{
-                return get_file_name!(file);
-            }
-            file.display().to_string()
-        }
-    }).collect();
-
+        })
+        .collect();
 
     let grid = Grid::new(
-        file_names, 
+        file_names,
         GridOptions {
             filling: term_grid::Filling::Spaces(2),
             direction: term_grid::Direction::TopToBottom,
-            width: stdout_width
-        }
+            width: stdout_width,
+        },
     );
 
     println!("{grid}");
 }
 
-
-pub fn setup_logging() -> Result<bool, Error>{
-    if let Err(e) = std::fs::create_dir_all(LOG_DIR){
+pub fn setup_logging() -> Result<bool, Error> {
+    if let Err(e) = std::fs::create_dir_all(LOG_DIR) {
         eprintln!("Failed to create logging directory {}: {}", LOG_DIR, e);
         return Err(e);
     }
@@ -93,19 +102,21 @@ pub fn setup_logging() -> Result<bool, Error>{
     let log_path = PathBuf::from(LOG_DIR);
     let history_path = log_path.join("history");
 
-    if !history_path.try_exists().unwrap(){
+    if !history_path.try_exists().unwrap() {
         std::fs::File::create(history_path).unwrap();
     }
 
     Ok(true)
 }
 
-pub fn setup_directory(args:&Args) -> Result<PathBuf, Error>{
+pub fn setup_directory(args: &Args) -> Result<PathBuf, Error> {
     let dir: String;
-    let mut var_dir: String = String::new(); 
+    let mut var_dir: String = String::new();
 
     match std::env::var("XDG_DATA_HOME") {
-        Ok(default_dir) => { var_dir = default_dir; }
+        Ok(default_dir) => {
+            var_dir = default_dir;
+        }
         Err(_) => {}
     }
 
@@ -113,14 +124,14 @@ pub fn setup_directory(args:&Args) -> Result<PathBuf, Error>{
         dir = args.dir.clone();
     } else if !var_dir.is_empty() {
         dir = var_dir;
-    } else{
+    } else {
         dir = args.dir.clone();
     }
 
-    let dir_path = match PathBuf::from(&dir).canonicalize(){
+    let dir_path = match PathBuf::from(&dir).canonicalize() {
         Ok(dir) => dir,
         Err(_) => {
-            if let Err(e) = fs::create_dir_all(&dir){
+            if let Err(e) = fs::create_dir_all(&dir) {
                 eprintln!("Failed to create directory {}: {}", dir, e);
                 return Err(e);
             }
@@ -129,21 +140,22 @@ pub fn setup_directory(args:&Args) -> Result<PathBuf, Error>{
     };
 
     if args.debug {
-        println!("Temporary Directory Path: {}", dir_path.display().to_string());
+        println!(
+            "Temporary Directory Path: {}",
+            dir_path.display().to_string()
+        );
     }
 
     Ok(dir_path)
 }
 
-
-
 /// This does the following
-/// 
+///
 /// 1. Create a info file, which stores the name and time at which it was moved here
 /// 2. Move the file
 /// TODO: Refactor the logic for finding new file path and transfer the move logic back to the main file
-pub fn move_files(args: &Args, dir_path: &PathBuf, files: &Vec<PathBuf>){
-    for file in files{
+pub fn move_files(args: &Args, dir_path: &PathBuf, files: &Vec<PathBuf>) {
+    for file in files {
         let full_path = match file.canonicalize() {
             Ok(path) => path,
             Err(e) => {
@@ -155,98 +167,163 @@ pub fn move_files(args: &Args, dir_path: &PathBuf, files: &Vec<PathBuf>){
         let mut new_location = dir_path.join(full_path.strip_prefix("/").unwrap());
 
         // ensuring parent directories exist
-        if let Some(parent) = new_location.clone().parent(){
-
+        if let Some(parent) = new_location.clone().parent() {
             // if name conflict exists, find the version number in a 2 step process
             // 1. Binary exponentiation to find upper limit
             // 2. Binary search to find actual number
-            if parent.exists(){
+            if new_location.exists() {
                 let mut search_start = 1;
                 let mut search_end = 1;
                 let file_name = get_file_name!(file);
-                while parent.join(format!("{}_{}", file_name, &search_end.to_string())).exists(){
+                while parent
+                    .join(format!("{}_{}", file_name, &search_end.to_string()))
+                    .exists()
+                {
                     search_start = search_end;
                     search_end *= 2;
                 }
-                
 
-                // Binary search 
-                while search_start < search_end{
+                // Binary search
+                while search_start < search_end {
                     let middle = (search_start + search_end) / 2;
-                    let curr_file_name = parent.join(format!("{}_{}", file_name, &middle.to_string()));
-                    
-                    if curr_file_name.exists(){
-                        search_start = middle+1;
+                    let curr_file_name =
+                        parent.join(format!("{}_{}", file_name, &middle.to_string()));
+
+                    if curr_file_name.exists() {
+                        search_start = middle + 1;
                     } else {
                         search_end = middle;
                     }
-                    
                 }
 
                 let new_file_name = format!("{}_{}", file_name, search_end);
                 new_location.set_file_name(&new_file_name);
 
-                if args.debug{
+                if args.debug {
                     println!("New file name: {}", new_file_name);
                     println!("New file location: {}", new_location.display());
                 }
             }
 
-            if let Err(e) = fs::create_dir_all(parent){
+            if let Err(e) = fs::create_dir_all(parent) {
                 eprintln!("Failed to create directory {}: {}", parent.display(), e);
                 std::process::exit(1);
             }
         }
 
-        if args.debug{
+        if args.debug {
             println!("New file location: {}", new_location.display());
         }
 
-
         match fs::rename(&full_path, &new_location) {
-            Ok(_) => (),
+            Ok(_) => {
+                if args.verbose {
+                    println!("Successfully moved {} to trash", full_path.display());
+                }
+            }
             Err(e) => {
-                eprintln!("Failed to move files from {} to {}: {}", full_path.display(), new_location.display(), e);
+                eprintln!(
+                    "Failed to move files from {} to {}: {}",
+                    full_path.display(),
+                    new_location.display(),
+                    e
+                );
                 std::process::exit(1);
             }
         }
-    } 
+    }
 }
 
-pub fn list_delete_files(args: &Args, dir_path: &PathBuf, files: &mut Vec<PathBuf>){
+pub fn list_delete_files(
+    args: &Args,
+    dir_path: &PathBuf,
+    files: &mut Vec<PathBuf>,
+    return_list: bool,
+) -> Result<Vec<Vec<PathBuf>>, Error> {
     let cwd = std::env::current_dir().unwrap();
-    if files.is_empty(){
+    if files.is_empty() {
         files.push(cwd.clone());
     }
+
+    let mut deleted_files: Vec<Vec<PathBuf>> = vec![];
 
     for file in files.iter_mut() {
         let full_path = match file.canonicalize() {
             Ok(path) => path,
-            Err(_) => {
-                cwd.clone().join(&file)
-                // return;
-            }
+            Err(_) => cwd.clone().join(&file),
         };
+
         *file = dir_path.join(full_path.strip_prefix("/").unwrap());
 
-
         let sub_files: Vec<PathBuf> = match fs::read_dir(&file) {
-            Ok(entries) => entries.filter_map(|entry| entry.ok().map(|e| e.path())).collect(),
+            Ok(entries) => entries
+                .filter_map(|entry| entry.ok().map(|e| e.path()))
+                .collect(),
             Err(e) => {
                 eprintln!("Failed to read directory {}: {}", file.display(), e);
-                return;
+                return Err(e);
             }
         };
 
+        if return_list {
+            deleted_files.push(sub_files.clone());
+        }
 
-        println!("{}:", file.display().to_string());
-        display_files(&sub_files, true);
-
+        if sub_files.len() == 0 {
+            println!("No files found under {}", file.display());
+        } else {
+            println!("{}:", file.display().to_string());
+            display_files(&sub_files, true);
+        }
     }
-    
-    if args.debug{
+
+    if args.debug {
         println!("Len of files: {}", files.len());
     }
 
+    if return_list {
+        return Ok(deleted_files);
+    }
 
+    Ok(vec![])
+}
+
+pub fn recover_files(args: &Args, dir_path: &PathBuf, files: &mut Vec<PathBuf>, from_trash: bool) {
+    let cwd = std::env::current_dir().unwrap();
+
+    for file in files.iter_mut() {
+        let mut full_path = match file.canonicalize() {
+            Ok(path) => path,
+            Err(_) => cwd.clone().join(&file),
+        };
+        if !from_trash {
+            *file = dir_path.join(full_path.strip_prefix("/").unwrap());
+        } else {
+            full_path = PathBuf::from("/").join(full_path.strip_prefix(dir_path).unwrap());
+        }
+
+        if file.exists() {
+            match fs::rename(&file, &full_path) {
+                Ok(_) => {
+                    if args.verbose {
+                        println!(
+                            "Successfully recovered file from trash to {}",
+                            full_path.display()
+                        );
+                    }
+                }
+                Err(e) => {
+                    eprintln!(
+                        "Failed to move files from {} to {}: {}",
+                        file.display(),
+                        full_path.display(),
+                        e
+                    );
+                    std::process::exit(1);
+                }
+            }
+        } else {
+            eprintln!("Unable to move {}: No such file exists", file.display());
+        }
+    }
 }
